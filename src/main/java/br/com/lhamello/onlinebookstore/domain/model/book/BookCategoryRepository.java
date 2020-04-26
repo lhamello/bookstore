2ceb68a0f62e438b0c265f5839d55d083477fdea
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-04-26 13:42:54 -0300

    Alterado path rest resource.

--- src/main/java/br/com/lhamello/onlinebookstore/domain/model/book/BookCategoryRepository.java
+++ src/main/java/br/com/lhamello/onlinebookstore/domain/model/book/BookCategoryRepository.java
@@ -1,7 +1,9 @@
 package br.com.lhamello.onlinebookstore.domain.model.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "bookCategories", path = "book-categories")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 
 }
