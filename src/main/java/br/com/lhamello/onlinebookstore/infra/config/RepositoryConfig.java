964c4fdc6248171381e595f51672a142a8f20e8e
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-04-26 15:10:00 -0300

    Alterado a forma de expor o ID das classes.

--- src/main/java/br/com/lhamello/onlinebookstore/infra/config/RepositoryConfig.java
+++ src/main/java/br/com/lhamello/onlinebookstore/infra/config/RepositoryConfig.java
@@ -1,18 +1,28 @@
 package br.com.lhamello.onlinebookstore.infra.config;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
-import br.com.lhamello.onlinebookstore.domain.model.book.Book;
-import br.com.lhamello.onlinebookstore.domain.model.book.BookCategory;
-
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer {
 
+  private final EntityManager entityManager;
+  
+  public RepositoryConfig(final EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
+  
   @Override
   public void configureRepositoryRestConfiguration(final RepositoryRestConfiguration config) {
-    config.exposeIdsFor(Book.class);
-    config.exposeIdsFor(BookCategory.class);
+    config.exposeIdsFor(
+        entityManager.getMetamodel()
+                     .getEntities()
+                     .stream()
+                     .map(Type::getJavaType)
+                     .toArray(Class[]::new));
   }
 }
