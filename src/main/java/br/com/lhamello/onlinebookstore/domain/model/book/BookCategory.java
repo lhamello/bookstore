d0581ac6bf04cad0da823b4560635f11a7fcb362
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-04-26 15:00:33 -0300

    Adicionado getters and setters.

--- src/main/java/br/com/lhamello/onlinebookstore/domain/model/book/BookCategory.java
+++ src/main/java/br/com/lhamello/onlinebookstore/domain/model/book/BookCategory.java
@@ -23,6 +23,30 @@ public class BookCategory {
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
   private Set<Book> books;
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Set<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(Set<Book> books) {
+    this.books = books;
+  }
+
   @Override
   public String toString() {
     return new ToStringCreator(this)
