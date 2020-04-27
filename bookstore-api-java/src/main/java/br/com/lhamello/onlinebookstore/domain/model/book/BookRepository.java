3401fe29c305dff1cae36b7bac53df391c0640b2
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-04-27 00:28:54 -0300

    Adicionado annotation @CrossOrigin.

--- bookstore-api-java/src/main/java/br/com/lhamello/onlinebookstore/domain/model/book/BookRepository.java
+++ bookstore-api-java/src/main/java/br/com/lhamello/onlinebookstore/domain/model/book/BookRepository.java
@@ -1,7 +1,9 @@
 package br.com.lhamello.onlinebookstore.domain.model.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
